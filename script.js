7a6d5767497dca545ee6c695ceacb2394c9ad746
TPride <tranielmp8@gmail.com>
2023-04-25 13:13:45 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// // function to send email:
+// function sendMsg(e){
+//   e.preventDefault();
+//   const form = document.querySelector('.contact-form'), name = document.querySelector('.name'), email = document.querySelector('.email'), msg = document.querySelector('.msg');
+
+//   // function send email
+//   Email.send({
+//     SecureToken : "",
+//     To : 'them@website.com',
+//     From : email.value,
+//     Subject : "WebDev Contact Form",
+//     Body : "And this is the body"
+//   }).then(
+//   message => alert(message)
+//   );
+// }
+
